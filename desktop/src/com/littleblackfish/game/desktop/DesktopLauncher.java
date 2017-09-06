d1ea7d44d3ec0d4e5414bf0389e10f23fd9c4c32
codingBird <baran.mohammadi17@gmail.com>
2017-09-06 17:11:47 +0430

    creat project

---
+++ desktop/src/com/littleblackfish/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.littleblackfish.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.littleblackfish.game.LittleBlackFishGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LittleBlackFishGame(), config);
+	}
+}
