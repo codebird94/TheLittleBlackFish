e2f87930896addbcc74b1773d9317e4c3aab0de3
codingBird <baran.mohammadi17@gmail.com>
2017-09-06 17:23:08 +0430

    add the whole project in git

---
+++ ios/src/com/littleblackfish/game/IOSLauncher.java
@@ -0,0 +1,22 @@
+package com.littleblackfish.game;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import com.littleblackfish.game.LittleBlackFishGame;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        return new IOSApplication(new LittleBlackFishGame(), config);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}
